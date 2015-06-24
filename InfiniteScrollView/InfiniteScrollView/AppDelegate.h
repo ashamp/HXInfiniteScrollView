28d7da7cf56297b3170c543f2f6a3c2a225340a6
谢俊伟 <xiejunwei@xiejunweideMac-mini.local>
2015-06-24 13:56:17 +0800

    init

---
+++ InfiniteScrollView/InfiniteScrollView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  InfiniteScrollView
+//
+//  Created by 谢俊伟 on 6/24/15.
+//  Copyright (c) 2015 XieJunwei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ InfiniteScrollView/InfiniteScrollView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  InfiniteScrollView
+//
+//  Created by 谢俊伟 on 6/24/15.
+//  Copyright (c) 2015 XieJunwei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
